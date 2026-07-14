5c5bd1224ee5582d6dc6233bee9f8f65c25b5585
David Lezcano <david.dlb02@gmail.com>
2026-07-14 11:14:24 -0400

    listo move

--- System/movement_system.gd
+++ System/movement_system.gd
@@ -7,6 +7,7 @@ extends Node
 signal move_finished()
 
 var is_moving = false
+var current_unit: Player
 
 func setup(
 	grid_system : GridSystem,
@@ -18,13 +19,18 @@ func setup(
 func move_unit(unit: Player, path: Array[Vector2i]) -> void:
 	var steps := path.duplicate()
 	is_moving = true
+	current_unit = unit
 
 	for cell in steps:
 		if !(is_moving and grid_system.is_cell_free(cell)):
 			return
 
+		var from: Vector2i = unit.grid_position
 		grid_system.move_entity(unit, cell)
 		var target_pos: Vector2 = grid_service.grid_to_world(cell)
+
+		var direction: Vector2i = cell - from
+		unit.play_walk(direction)
 
 		var tween := create_tween()
 		tween.tween_property(unit, "global_position", target_pos, 0.25)
@@ -34,6 +40,9 @@ func move_unit(unit: Player, path: Array[Vector2i]) -> void:
 		await get_tree().create_timer(0.25).timeout
 
 	is_moving = false
+	unit.idle()
 
 func stop_move():
 	is_moving = false
+	if current_unit:
+		current_unit.idle()
--- player.gd
+++ player.gd
@@ -3,3 +3,35 @@ extends Node2D
 
 var grid_position : Vector2i = Vector2i.ZERO
 var alert: bool = false
+var facing_direction: Vector2i = Vector2i.DOWN
+@onready var animSprite = $AnimatedSprite2D
+
+func _ready() -> void:
+	animSprite.play("idle_down")
+
+
+func play_walk(direction: Vector2i) -> void:
+	if direction == Vector2i.ZERO:
+		return
+	facing_direction = direction
+	match direction:
+		Vector2i.UP:
+			animSprite.play("up")
+		Vector2i.DOWN:
+			animSprite.play("down")
+		Vector2i.LEFT:
+			animSprite.play("left")
+		Vector2i.RIGHT:
+			animSprite.play("right")
+
+
+func idle() -> void:
+	match facing_direction:
+		Vector2i.UP:
+			animSprite.play("idle_up")
+		Vector2i.DOWN:
+			animSprite.play("idle_down")
+		Vector2i.LEFT:
+			animSprite.play("idle_left")
+		Vector2i.RIGHT:
+			animSprite.play("idle_right")
