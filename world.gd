97fc57a783594bbb45fdfd4920f2b3cd055e62bb
David Lezcano <david.dlb02@gmail.com>
2026-07-17 15:24:30 -0400

    marca obstaculos desde tiles

--- world.gd
+++ world.gd
@@ -17,27 +17,22 @@ extends Node2D
 var turn := 0
 var hover: Vector2i = Vector2i.ZERO
 
-var obstacles: Array[Obstacle] = [
-	Obstacle.new_at(Vector2i(1,3)),
-	Obstacle.new_at(Vector2i(3,1)),
-	Obstacle.new_at(Vector2i(2,1)),
-	Obstacle.new_at(Vector2i(0,4))
-]
+var obstacles: Array[Obstacle] = []
 
 func _ready():
-	create_obstacles()
-	
+	load_tile_obstacles()
+
 	grid_service.setup(tilemap)
-	path_service.setup(Vector2i(20,20),Vector2(32,32),obstacles)
+	path_service.setup(Vector2i(20, 20), Vector2(32, 32), obstacles)
 	path_system.setup(path_service)
 	preview_service.setup(grid_service)
-	preview_system.setup(path_system,preview_service)
+	preview_system.setup(path_system, preview_service)
 	movement_system.setup(grid_system, grid_service)
-	
+
 	player.grid_position = grid_service.world_to_grid(player.global_position)
 	grid_system.register_entity(player)
 	turn_system.register(player)
-	
+
 	movement_system.move_finished.connect(turn_system.end_turn)
 	turn_system.turn_started.connect(_on_turn_started)
 
@@ -46,14 +41,14 @@ func _ready():
 func _on_turn_started(entity: Player):
 	turn += 1
 	label.text = "Turno: %d" % turn
-	
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		var cell = grid_service.world_to_grid(get_global_mouse_position())
 
 		if !movement_system.is_moving and cell != hover:
 			hover = cell
-			preview_system.update_preview(player,cell)
+			preview_system.update_preview(player, cell)
 
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		if movement_system.is_moving:
@@ -66,17 +61,10 @@ func _unhandled_input(event):
 
 			preview_system.clear()
 
-func create_obstacles():
-	for obs in obstacles:
-		var rect := ColorRect.new()
-
-		rect.color = Color.DARK_RED
-		rect.size = Vector2(32,32)
-		rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
-
-		rect.position = (
-			Vector2(obs.grid_position) * 32
-		)
-
-		add_child(rect)
-		grid_system.register_entity(obs)
+func load_tile_obstacles():
+	for cell in tilemap.get_used_cells():
+		var tile_data = tilemap.get_cell_tile_data(cell)
+		if tile_data and tile_data.get_collision_polygons_count(0) > 0:
+			var obs = Obstacle.new_at(cell)
+			obstacles.append(obs)
+			grid_system.register_entity(obs)
